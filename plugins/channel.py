28f95c2ffdc425c473a069d6b20879bc9433be2e
ABHINAND A S <abhinandunnikuttan04@gmail.com>
2024-02-10 20:09:13 +0530

    Add files via upload

---
+++ plugins/channel.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters
+from info import CHANNELS
+from database.ia_filterdb import save_file
+
+media_filter = filters.document | filters.video
+
+EXCLUDED_KEYWORDS = ["predvd", "Predvdrip", "hdcamrip", "apk", "sample", "camrip", "hdcam"]
+
+@Client.on_message(filters.chat(CHANNELS) & media_filter)
+async def media(bot, message):
+    """Media Handler"""
+    for file_type in ("document", "video"):
+        media = getattr(message, file_type, None)
+        if media is not None:
+            break
+    else:
+        return
+
+    media.file_type = file_type
+    media.caption = message.caption
+
+    # Check if the file size is greater than or equal to 35MB (35000000 bytes)
+    if media.file_size and media.file_size >= 35000000:
+        if any(keyword in media.caption.lower() for keyword in EXCLUDED_KEYWORDS):
+            return  # Skip if any excluded keyword is present in the caption
+        if media.file_name and any(keyword in media.file_name.lower() for keyword in EXCLUDED_KEYWORDS):
+            return  # Skip if any excluded keyword is present in the file name
+        await save_file(media)
